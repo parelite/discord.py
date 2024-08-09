2f9a1384c45bd1b1ebe5164df0188666062752b0
Samuel <41790962+parelite@users.noreply.github.com>
2024-08-09 23:22:02 +0100

    example for fiji

--- discord/ext/commands/core.py
+++ discord/ext/commands/core.py
@@ -428,6 +428,7 @@ class Command(_BaseCommand, Generic[CogT, P, T]):
 
         self.brief: Optional[str] = kwargs.get('brief')
         self.usage: Optional[str] = kwargs.get('usage')
+        self.example: Optional[str] = kwargs.get('example')
         self.rest_is_raw: bool = kwargs.get('rest_is_raw', False)
         self.aliases: Union[List[str], Tuple[str]] = kwargs.get('aliases', [])
         self.extras: Dict[Any, Any] = kwargs.get('extras', {})
